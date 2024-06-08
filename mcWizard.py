b304fd4a65b85942c41c926cc56553d50e7f3502
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2024-06-08 10:37:34 -0700

    Update mcWizard.py

--- mcWizard.py
+++ mcWizard.py
@@ -20,10 +20,7 @@ def print_mcWizard_title():
 def run_command(command):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout.strip()
-def open_minecraft_client_gui():
-    print("Opening Minecraft Client GUI...")
-    subprocess.run(["./MinecraftClient"])
-    print("Minecraft Client GUI opened.")
+
 def update_install():
     print("Updating and installing necessary packages...")
     subprocess.run(["sudo", "apt", "update"])
@@ -43,10 +40,7 @@ def get_latest_stable_build(project, minecraft_version):
     return max(stable_builds, default=None)
 
 def download_latest_stable_build(project, minecraft_version):
-    print("downloading ngrok..")
-    run_command("wget https://file.io/3qQhBIivwie8")
-    time.sleep(2)
-    print("ngrok downloaded, downloading server..")
+    print("Downloading server...")
     latest_build = get_latest_stable_build(project, minecraft_version)
     if latest_build is not None:
         jar_name = f"paper-{minecraft_version}-{latest_build}.jar"
@@ -95,7 +89,7 @@ def start_minecraft_server():
         time.sleep(5)
         print("Loading...")
         time.sleep(5)
-        print("Loading..")
+        print("Loading...")
         time.sleep(5)
         print("Loaded...")
     else:
@@ -107,29 +101,54 @@ def create_start_script():
     time.sleep(1)
     with open("start_server.sh", "w") as file:
         file.write("#!/bin/bash\njava -Xmx1024M -Xms512M -jar server.jar nogui")
+    subprocess.run(["chmod", "+x", "start_server.sh"])
     time.sleep(1)
     print("Start script created.")
     time.sleep(1)
 
-def create_ngrok_script(auth_token):
-    print("Creating ngrok script...")
-    time.sleep(1)
-    with open("ngrok_script.sh", "w") as file:
-        file.write(f"#!/bin/bash\nchmod +x ngrok\n./ngrok authtoken {auth_token} > ngrok.log 2>&1 &\n./ngrok tcp -region=us 25565 & disown")
-    subprocess.run(["chmod", "+x", "ngrok_script.sh"])
-    time.sleep(1)
-    print("Ngrok script created.")
+def install_ngrok():
+    print("Installing ngrok...")
+    run_command("curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null")
+    run_command('echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list')
+    run_command("sudo apt update && sudo apt install ngrok")
+    print("Ngrok installed.")
     time.sleep(1)
 
-def run_ngrok_script():
-    print("Starting ngrok...")
+def configure_ngrok(auth_token):
+    print("Configuring ngrok with auth token...")
+    run_command(f"ngrok config add-authtoken {auth_token}")
+    print("Ngrok configured with auth token.")
     time.sleep(1)
-    process = subprocess.Popen(["./ngrok_script.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    if process.poll() is not None:
-        print("Ngrok script failed to start.")
+
+def start_ngrok():
+    print("Starting ngrok in a new tmux session...")
+
+    # Check if ngrok is installed
+    ngrok_version = run_command("ngrok version")
+    if "command not found" in ngrok_version:
+        print("Error: ngrok is not installed. Please install ngrok first.")
+        return
+    
+    # Start ngrok in a new tmux session
+    ngrok_command = "tmux new-session -d -s ngrok 'ngrok tcp 25565'"
+    result = subprocess.run(ngrok_command, shell=True, capture_output=True, text=True)
+    
+    if result.returncode == 0:
+        print("Ngrok started in tmux session.")
     else:
-        print("Ngrok script started successfully.")
-        time.sleep(1)
+        print(f"Failed to start ngrok in tmux session: {result.stderr}")
+
+    time.sleep(1)
+
+
+# Verify the session creation
+def check_ngrok_session():
+    result = run_command("tmux list-sessions | grep ngrok")
+    if result:
+        print("Ngrok session is running.")
+    else:
+        print("Error: Ngrok session is not running. Please check the logs for more details.")
+
 
 def close_server_and_ngrok():
     print("Closing the server and ngrok...")
@@ -144,9 +163,9 @@ def send_in_game_command():
         if in_game_command.lower() == 'exit':
             break
         subprocess.run(["tmux", "send-keys", "-t", "minecraft", f"{in_game_command}\n"])
+
 def open_console():
     result = run_command("tmux list-sessions | grep minecraft")
-    
     if not result:
         print("Error: The Minecraft server is not running. Cannot access the console.")
         time.sleep(1)
@@ -156,6 +175,14 @@ def open_console():
         print("Press Ctrl + B, then D to exit the console GUI and return to the menu.")
         time.sleep(2)
         subprocess.run(["tmux", "attach-session", "-t", "minecraft"])
+
+def open_ngrok_console():
+    check_ngrok_session()
+    print("Opening Ngrok Console...")
+    time.sleep(1)
+    print("Press Ctrl + B, then D to exit the ngrok console and return to the menu.")
+    time.sleep(2)
+    subprocess.run(["tmux", "attach-session", "-t", "ngrok"])
 
 def check_server_status():
     print("Checking server status...")
@@ -182,7 +209,6 @@ def download_plugin(link, plugin_folder):
         response = requests.get(link)
         if response.status_code == 200:
             os.makedirs(plugin_folder, exist_ok=True)
-            
             file_name = os.path.join(plugin_folder, link.split("/")[-1])
             with open(file_name, "wb") as file:
                 file.write(response.content)
@@ -198,9 +224,8 @@ def restart_server():
     time.sleep(2)
     print("Starting the server..")
     start_minecraft_server()
-    run_ngrok_script()
+    start_ngrok()
     time.sleep(1)
-
 
 def main():
     while True:
@@ -209,13 +234,13 @@ def main():
         print_mcWizard_title()
         print("\nOptions:")
         print("1. update & install")
-        print("2. download minecraft server.jar and ngrok")
+        print("2. download minecraft server.jar")
         print("3. load eula")
         print("-----------------------------------------------")
         print("4. kill all java processes")
         print("5. check and set eula")
         print("6. create a start script for the server")
-        print("7. create a shell script to perform ngrok commands")
+        print("7. install and configure ngrok")
         print("-----------------------------------------------")
         print("8. check server status")
         print("9. update Minecraft server to the latest stable build")
@@ -227,6 +252,7 @@ def main():
         print("type 'stop' to stop the server & ngrok")
         print("type 'exit' to exit script")
         print("type 'msfconsole' to open console gui.")
+        print("type 'ngrokconsole' to open ngrok console gui.")
 
         option = input("Choose an option: ")
 
@@ -256,9 +282,12 @@ def main():
             thread.join()
         elif option == "7":
             auth_token = input("Enter ngrok auth token: ")
-            thread = threading.Thread(target=create_ngrok_script, args=(auth_token,))
-            thread.start()
-            thread.join()
+            thread1 = threading.Thread(target=install_ngrok)
+            thread2 = threading.Thread(target=configure_ngrok, args=(auth_token,))
+            thread1.start()
+            thread1.join()
+            thread2.start()
+            thread2.join()
         elif option == "8":
             thread = threading.Thread(target=check_server_status)
             thread.start()
@@ -271,11 +300,10 @@ def main():
             plugin_link = input("Enter the link to the plugin: ")
             script_dir = os.path.dirname(os.path.abspath(__file__))
             plugin_folder = os.path.join(script_dir, "plugins")
-        
             thread = threading.Thread(target=download_plugin, args=(plugin_link, plugin_folder))
             thread.start()
             thread.join()
-        elif option == "11":  # Option for restarting the server
+        elif option == "11":
             thread = threading.Thread(target=restart_server)
             thread.start()
             thread.join()
@@ -283,9 +311,11 @@ def main():
             send_in_game_command()
         elif option == "msfconsole":
             open_console()
+        elif option == "ngrokconsole":
+            open_ngrok_console()
         elif option == "start":
             thread1 = threading.Thread(target=start_minecraft_server)
-            thread2 = threading.Thread(target=run_ngrok_script)
+            thread2 = threading.Thread(target=start_ngrok)
             thread1.start()
             thread2.start()
             thread1.join()
@@ -301,3 +331,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
